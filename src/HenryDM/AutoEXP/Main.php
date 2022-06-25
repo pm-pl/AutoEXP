04e11da2a92d14d514e35f5648d2ab47241147dc
HenryDM <100979680+Henry12960@users.noreply.github.com>
2022-06-25 10:17:21 -0300

    OK Jason

--- src/HenryDM/AutoEXP/Main.php
+++ src/HenryDM/AutoEXP/Main.php
@@ -12,17 +12,6 @@ use pocketmine\plugin\PluginBase;
 class Main extends PluginBase implements Listener {
 
 	public function onEnable() : void {
-		$this->getServer()->getPluginManager()->registerEvents($this, $this);
-		$this->getServer()->getLogger()->info("                _        ________   _______  ");
-		$this->getServer()->getLogger()->info("     /\        | |      |  ____\ \ / /  __ \ ");
-		$this->getServer()->getLogger()->info("    /  \  _   _| |_ ___ | |__   \ V /| |__) |");
-	        $this->getServer()->getLogger()->info("   / /\ \| | | | __/ _ \|  __|   > < |  ___/ ");
-		$this->getServer()->getLogger()->info("  / ____ \ |_| | || (_) | |____ / . \| |     ");
-		$this->getServer()->getLogger()->info(" /_/    \_\__,_|\__\___/|______/_/ \_\_|     ");
-		$this->getServer()->getLogger()->info("");
-		$this->getServer()->getLogger()->info("By HenryDM");
-	        $this->getServer()->getLogger()->info("Support: https://github.com/Henry12960");
-		$this->getServer()->getLogger()->info("Plugin Enable!");
 	}
 
 	/**
