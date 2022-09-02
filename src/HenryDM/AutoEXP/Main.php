61783d65b826951b188a070cf5c1ee40c4637aae
HenryDM <100979680+Henry12960@users.noreply.github.com>
2022-09-02 07:46:20 -0300

    Add Features and rewrite code
    Improve functions:
    
    • Enable or disable Auto EXP on Kill or on Break blocks
    
    • Add PerWorld functions

--- src/HenryDM/AutoEXP/Main.php
+++ src/HenryDM/AutoEXP/Main.php
@@ -13,34 +13,48 @@ class Main extends PluginBase implements Listener {
 
 	public function onEnable() : void {
             $this->getServer()->getPluginManager()->registerEvents($this, $this);
+            $this->saveDefaultConfig();
 	}
 
 	/**
 	 * @param BlockBreakEvent $event
-	 * @priority HIGHEST
-	 */
-	public function onBreak(BlockBreakEvent $event): void{
-		if ($event->isCancelled()) {
-			return;
-		}
-		$event->getPlayer()->getXpManager()->addXp($event->getXpDropAmount());
-		$event->setXpDropAmount(0);
-	}
+         * priority HIGHEST
+         */
 
+	public function onBreak(BlockBreakEvent $event): void {
+.              if($this->getConfig()->get("auto-block-exp") === true) {
+                  $player = $event->getPlayer();
+                  $world = $player->getWorld();
+                  $worldName = $world->getFolderName();
+	          if ($event->isCancelled()) {
+		     return;
+		}
+                     if (in_array($worldName, $this->getConfig()->get("block-exp-worlds"))) {
+                         $event->getPlayer()->getXpManager()->addXp($event->getXpDropAmount());
+	                 $event->setXpDropAmount(0);
+	    }
+          }
+        }
 
 	/**
 	 * @param PlayerDeathEvent $event
 	 * @priority HIGHEST
 	 */
-	public function onPlayerKill(PlayerDeathEvent $event): void{
-		$player = $event->getPlayer();
-		$cause = $player->getLastDamageCause();
-		if ($cause instanceof EntityDamageByEntityEvent) {
-			$damager = $cause->getDamager();
-			if ($damager instanceof Player) {
-				$damager->getXpManager()->addXp($player->getXpDropAmount());
-				$player->getXpManager()->setCurrentTotalXp(0);
-			}
-		}
-	}
+
+	public function onKill(PlayerDeathEvent $event): void {
+               if($this->getConfig()->get("auto-kill-exp") === true) {
+		  $player = $event->getPlayer();
+		  $cause = $player->getLastDamageCause();
+                  $world = $player->getWorld();
+                  $worldName = $world->getFolderName();
+		  if ($cause instanceof EntityDamageByEntityEvent) {
+		       $damager = $cause->getDamager();
+		       if ($damager instanceof Player) {
+		           $damager->getXpManager()->addXp($player->getXpDropAmount());
+			   $player->getXpManager()->setCurrentTotalXp(0);
+
+            }
+         }
+      } 
+   }
 }
